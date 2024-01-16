3f1e96f6f7487b5b94616e54cfcf6c6d0ea2b766
JMHS Robotics Laptop 2 <61028962+frc8243@users.noreply.github.com>
2024-01-16 17:25:32 -0500

    Shoot and Intake change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -141,7 +141,7 @@ public class Constants {
     public class ShooterConstants {
         public static final int kShootMotorID = 2;
         public static final int kFeedMotorID = 3;
-        public static final double kFeedSpeed = 0.25;
-        public static final double kShootSpeed = 0.25;
+        public static final double kFeedSpeed = 1;
+        public static final double kShootSpeed = 1;
     }
 }
