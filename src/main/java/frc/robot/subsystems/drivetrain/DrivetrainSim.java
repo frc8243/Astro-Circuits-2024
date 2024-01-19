6753a191c140302798d63d5946f8a8343828ecd8
JulienM14 <julienmedina314@gmail.com>
2024-01-18 23:49:25 -0500

    rewritten gyro with accessor methods

--- src/main/java/frc/robot/subsystems/drivetrain/DrivetrainSim.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DrivetrainSim.java
@@ -7,50 +7,37 @@ public class DrivetrainSim implements DrivetrainIO {
 
     @Override
     public void drive(double x, double y, double rot, boolean fieldOriented, boolean rateLimit) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'drive'");
     }
 
     @Override
     public void setX() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setX'");
     }
 
     @Override
     public Pose2d getPose() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPose'");
+        return null;
     }
 
     @Override
     public void driveRobotRelative(ChassisSpeeds speeds) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'driveRobotRelative'");
     }
 
     @Override
     public ChassisSpeeds getRobotRelativeSpeeds() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRobotRelativeSpeeds'");
+        return null;
     }
 
     @Override
     public void resetOdometry(Pose2d pose) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resetOdometry'");
     }
 
     @Override
     public void resetEncoders() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'resetEncoders'");
     }
 
     @Override
     public void updateTelemetry() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'updateTelemetry'");
+
     }
 
 }
