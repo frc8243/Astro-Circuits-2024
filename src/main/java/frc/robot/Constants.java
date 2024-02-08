e79254ad5491e2b8ded520cc911e2b67b8fb8701
JMHS Robotics Laptop 2 <61028962+frc8243@users.noreply.github.com>
2024-02-08 16:12:18 -0500

    things

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -251,4 +251,8 @@ public class Constants {
     // distance per pulse = (distance per revolution) / (pulses per revolution)
     // = (Pi * D) / ppr
     public static final double kElevatorEncoderDistPerPulse = 2.0 * Math.PI * kElevatorDrumRadius / 4096;
+
+    public static final class LEDConstants {
+        public static final int kOutLimitSwitch = 1;
+    }
 }
--- src/main/java/frc/robot/subsystems/LEDs.java
+++ src/main/java/frc/robot/subsystems/LEDs.java
@@ -3,16 +3,20 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotContainer;
+import frc.robot.Constants.LEDConstants;
 
 public class LEDs extends SubsystemBase {
   // private final static AddressableLED LED = new AddressableLED(1);
   // private final static AddressableLEDBuffer LEDBuffer = new
   // AddressableLEDBuffer(60);
   Spark m_blinkin = new Spark(2);
+  DigitalInput photoSensor = new DigitalInput(0);
   double value = -1;
+  DigitalInput ledLimitSwitchShooter = new DigitalInput(LEDConstants.kOutLimitSwitch);
 
   /** Creates a new LEDs. */
   public LEDs() {
@@ -22,7 +26,18 @@ public class LEDs extends SubsystemBase {
 
   @Override
   public void periodic() {
-    m_blinkin.set(0.61);
+    if (!ledLimitSwitchShooter.get()) {
+      System.out.println("limit switch on");
+      m_blinkin.set(0.75);
+      // m_blinkin.set(0.35);
+    } else if (photoSensor.get()) {
+      m_blinkin.set(-0.01);
+    } else {
+      System.out.println("limit switch off");
+
+      m_blinkin.set(0.93);
+    }
+
     // LED.setData(LEDBuffer);
 
   }
